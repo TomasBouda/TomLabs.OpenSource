751643f8277c3e06e15f21f5b24d3579793a2cc0
Tomáš Bouda <tomas.bouda@linksoft.cz>
2017-05-03 21:56:00 +0200

    Add project files.

---
+++ TomLabs.OpenSource.Database/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomLabs.OpenSource.Database
+{
+    public class Class1
+    {
+    }
+}
